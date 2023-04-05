414dfd2d81afa4ca7769631732cca99263bca501
Bvder92 <badragrad@gmail.com>
2023-04-05 12:41:56 +0200

    css for login + profile

--- app/login.php
+++ app/login.php
@@ -1,31 +1,71 @@
-<h2>Se connecter</h2>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/style/style1.css">
+    <script src="https://kit.fontawesome.com/927b94a7cf.js" crossorigin="anonymous"></script>
+    <title>Se connecter</title>
+</head>
+<body>
+    <style>
+        body{
+            /*background-image: linear-gradient(90deg, rgba(201,63,63,1) 0%, rgba(0,0,0,1) 50%, rgba(201,63,63,1) 100%),url(images/background-login.jpeg);*/
+            width:100%;
+            height:100vh;
+            background-image: linear-gradient(1deg, rgba(0,0,0,1) 0%, rgba(126,159,195,0.3435749299719888) 100%),url('images/background-login.jpeg');
+            background-size: cover;
+            background-position: center;
+        }
+   
+    </style>
+        
+  
+    
 
-<form action="include/login_script.php" method="post">
 
-    <input type="text" name="email" placeholder="Adresse mail">
-    <input type="password" name="mdp" placeholder="Mot de passe">
-    <button type="submit" name="submit">Se connecter</button>
 
-</form>
 
+<div class="wrapper">
+    <div class="form-box login">
+        <h2>SE CONNECTER</h2>
+        <form  class="form" action="include/login_script.php" method="post">
+            <div class="input-box">
+                <span class="icon"><i class="fa-regular fa-envelope"></i></span>
+                <input class="mail" type="text" name="email" placeholder="Adresse mail"><br>
+                <label>Email</label>
+            </div>
+            <div class="input-box">
+                <span class="icon"><i class="fa-regular fa-lock"></i></span>
+                <input class="pwd" type="password" name="mdp" placeholder="Mot de passe"><br>
+                <label>Password</label>
+            </div>
+            <button type="submit" name="submit" class="btn">CONNEXION</button>
+        </form>
+    </div>
+</div>
 <!-- affichage des messages d'erreurs contenus dans l'url -->
 <?php
     if(isset($_GET["error"])){
        
         switch($_GET["error"]){
             case "emptyinput":
-                echo "<p><b>Erreur: Veuillez renseigner tous les champs!</b></p>";
+                echo "<p class='errror'><b>Erreur: Veuillez renseigner tous les champs!</b></p>";
                 break;
             case "unknownemail":
-                echo "<p><b>Erreur: Aucun compte ne correspond à cette adresse mail!</b></p>";
+                echo "<p class='errror'><b>Erreur: Aucun compte ne correspond à cette adresse mail!</b></p>";
                 break;
             case "wrongpwd":
-                echo "<p><b>Erreur: Mot de passe invalide!</b></p>";
+                echo "<p class='errror'><b>Erreur: Mot de passe invalide!</b></p>";
                 break;
             case "sqlerror":
-                echo "<p><b>Erreur: Une erreur est survenue, veuillez réessayer.</b></p>";
+                echo "<p class='errror'><b>Erreur: Une erreur est survenue, veuillez réessayer.</b></p>";
                 break;
         }
     }
 
 ?>
+
+</body>
+</html>
